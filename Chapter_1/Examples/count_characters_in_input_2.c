0b5da3608d10c193c7fa505aa026ccc2b337e367
SwiftySwizzy <mihailernandes@gmail.com>
2018-07-01 11:36:18 +0300

    Add character count code example 2

---
+++ Chapter_1/Examples/count_characters_in_input_2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* count characters in input 1st version */
+int main() {
+	
+	long char_count;
+
+	for (char_count = 0; getchar() != EOF; char_count++)
+		;
+	printf("Character count: %ld\n", char_count);
+
+	return 0;
+}
